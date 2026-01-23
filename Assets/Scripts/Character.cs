16d77d90751b146d97e7c3a61912dae66fee5d6c
wbalaniucGC <113048699+wbalaniucGC@users.noreply.github.com>
2026-01-23 09:43:26 -0500

    Removed TestEnemy

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Header("Character Stats")]
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private int maxHealth = 100;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
