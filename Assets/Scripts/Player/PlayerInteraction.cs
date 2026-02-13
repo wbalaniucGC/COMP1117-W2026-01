ec7ccc64739fcb7e68907232ccbf93fe57045a09
wbalaniucGC <113048699+wbalaniucGC@users.noreply.github.com>
2026-02-13 10:59:15 -0500

    Player Interaction Set Up

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
+using UnityEngine.InputSystem;
 
 public class PlayerInteraction : MonoBehaviour
 {
     [Header("Interactions Settings")]
     [SerializeField] private float interactRange = 1.5f;
     [SerializeField] private LayerMask interactableLayer;
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        // Fires once when pressed
+        if(context.started)
+        {
+            PerformInteraction();
+        }
+    }
+
+    private void PerformInteraction()
+    {
+        // Find everything in a circle around the fox on the "interactable" layer
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, interactRange, interactableLayer);
+
+        // Safety check
+        if(hit != null)
+        {
+            if(hit.TryGetComponent<IInteractable>(out IInteractable interactable))
+            {
+                interactable.Interact();
+                Debug.Log($"Interacted with {hit.name}");
+            }
+        }
+    }
 
     private void OnDrawGizmosSelected()
     {
